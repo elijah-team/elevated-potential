effa1e83c1d2e74216b8c8ffcfd6e2ab3912d9a6
Ttripleo Nova Sci <sci@tripleo.milkyway>
2023-09-06 18:12:59 -0400

    More Reactive in Garish

--- elijah-core/src/main/java/tripleo/elijah/stages/gen_c/WhyNotGarish_Class.java
+++ elijah-core/src/main/java/tripleo/elijah/stages/gen_c/WhyNotGarish_Class.java
@@ -2,18 +2,30 @@ package tripleo.elijah.stages.gen_c;
 
 import org.jdeferred2.impl.DeferredObject;
 import org.jetbrains.annotations.NotNull;
+import tripleo.elijah.nextgen.reactive.Reactivable;
+import tripleo.elijah.nextgen.reactive.ReactiveDimension;
 import tripleo.elijah.stages.gen_fn.EvaClass;
 import tripleo.elijah.stages.gen_generic.GenerateResultEnv;
+import tripleo.elijah.stages.gen_generic.pipeline_impl.GenerateResultSink;
 import tripleo.elijah.util.NotImplementedException;
+import tripleo.elijah.world.i.LivingClass;
+
+import static tripleo.elijah.ReactiveFlag.MANUAL_DISABLED;
 
 public class WhyNotGarish_Class implements WhyNotGarish_Item {
 	private final EvaClass                                 gc;
 	private final GenerateC                                generateC;
 	private final DeferredObject<GenerateResultEnv, Void, Void> fileGenPromise = new DeferredObject<>();
 
+	private final GCFC gcfc = new GCFC();
+
 	public WhyNotGarish_Class(final EvaClass aGc, final GenerateC aGenerateC) {
 		gc        = aGc;
 		generateC = aGenerateC;
+
+
+		gc.reactive().add(gcfc);
+
 
 		fileGenPromise.then(this::onFileGen);
 	}
@@ -24,6 +36,19 @@ public class WhyNotGarish_Class implements WhyNotGarish_Item {
 
 	private void onFileGen(final @NotNull GenerateResultEnv aFileGen) {
 		NotImplementedException.raise();
+
+		if (!MANUAL_DISABLED) {
+			gcfc.respondTo(this.generateC);
+		}
+
+		var gc1 = generateC._ce().getCompilation().livingRepo().getClass(gc).getGarish();
+
+		final @NotNull GenerateResultSink sink = aFileGen.resultSink();
+
+		if (sink != null)
+			sink.addClass_1(gc1, aFileGen.gr(), generateC);
+		else
+			System.err.println("sink failed");
 	}
 
 	@Override
@@ -34,5 +59,19 @@ public class WhyNotGarish_Class implements WhyNotGarish_Item {
 	@Override
 	public void provideFileGen(final GenerateResultEnv fg) {
 		fileGenPromise.resolve(fg);
+	}
+
+	public class GCFC implements Reactivable {
+
+		@Override
+		public void respondTo(final ReactiveDimension aDimension) {
+			if (aDimension instanceof GenerateC generateC) {
+				fileGenPromise.then(fileGen -> {
+					final LivingClass livingClass = generateC._ce().getCompilation().livingRepo().getClass(gc);
+
+					livingClass.getGarish().garish(generateC, fileGen.gr(), fileGen.resultSink());
+				});
+			}
+		}
 	}
 }
