fb18f647147edc69e769729ba3566f8c5d9d06d6
Ttripleo Nova Sci <sci@tripleo.milkyway>
2023-09-06 18:15:04 -0400

    Convert from record

--- elijah-stateful/src/main/java/tripleo/elijah/stateful/StateRegistrationToken.java
+++ elijah-stateful/src/main/java/tripleo/elijah/stateful/StateRegistrationToken.java
@@ -1,4 +1,35 @@
 package tripleo.elijah.stateful;
 
-public record StateRegistrationToken(int token) {
+import java.util.Objects;
+
+public final class StateRegistrationToken {
+	private final int token;
+
+	StateRegistrationToken(int token) {
+		this.token = token;
+	}
+
+	public int token() {
+		return token;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) return true;
+		if (obj == null || obj.getClass() != this.getClass()) return false;
+		StateRegistrationToken that = (StateRegistrationToken) obj;
+		return this.token == that.token;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(token);
+	}
+
+	@Override
+	public String toString() {
+		return "StateRegistrationToken[" +
+				"token=" + token + ']';
+	}
+
 }
