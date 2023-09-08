0e8c6475bf7b4a3189b3c7911c9abd3cc9b6e352
Tripleo Nova Sci <sci@tripleo.milkyway>
2023-09-08 09:08:37 -0400

    CB_Monitor something

--- elijah-core/src/main/java/tripleo/elijah/comp/i/ICompilationBus.java
+++ elijah-core/src/main/java/tripleo/elijah/comp/i/ICompilationBus.java
@@ -11,7 +11,7 @@ public interface ICompilationBus {
 
 	IProgressSink defaultProgressSink();
 
-	CompilerDriver getCompilationDriver();
+	CompilerDriver getCompilerDriver();
 
 	void inst(ILazyCompilerInstructions aLazyCompilerInstructions);
 
