67d9f2b5c1622aada6d205a70dacad15801d4e12
Tripleo Nova Sci <sci@tripleo.milkyway>
2023-09-08 08:33:44 -0400

    CR/CB_Action

--- elijah-core/src/main/java/tripleo/elijah/comp/internal/CB_Output.java
+++ elijah-core/src/main/java/tripleo/elijah/comp/internal/CB_Output.java
@@ -3,25 +3,12 @@ package tripleo.elijah.comp.internal;
 import org.jetbrains.annotations.NotNull;
 import tripleo.elijah.comp.i.CB_OutputString;
 
-import java.text.MessageFormat;
-import java.util.ArrayList;
 import java.util.List;
 
-public class CB_Output {
-	private final List<CB_OutputString> x = new ArrayList<>();
+public interface CB_Output {
+	@NotNull List<CB_OutputString> get();
 
-	public @NotNull List<CB_OutputString> get() {
-		return x;
-	}
+	void logProgress(int number, String text);
 
-	public void logProgress(final int number, final String text) {
-		if (number == 130) return;
-
-//		System.err.println
-		print(MessageFormat.format("{0} {1}", number, text));
-	}
-
-	void print(final String s) {
-		x.add(() -> s);
-	}
+	void print(String s);
 }
