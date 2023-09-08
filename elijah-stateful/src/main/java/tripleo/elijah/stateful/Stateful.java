e4cd14270d82ad7e3ef4f5df4f5e68b7c4c05c79
Tripleo Nova Sci <sci@tripleo.milkyway>
2023-09-08 09:03:44 -0400

    docs

--- elijah-stateful/src/main/java/tripleo/elijah/stateful/Stateful.java
+++ elijah-stateful/src/main/java/tripleo/elijah/stateful/Stateful.java
@@ -1,9 +1,16 @@
 package tripleo.elijah.stateful;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface Stateful {
-	void mvState(State aO, @NotNull State aState);
+	void mvState(@Nullable State aBeginState, @NotNull State aState);
 
+	/**
+	 * Set the internal state variable.
+	 * No other effects
+	 *
+	 * @param aState
+	 */
 	void setState(final State aState);
 }
