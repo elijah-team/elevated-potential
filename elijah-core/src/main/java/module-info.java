5aa3aede51c2c09135d8bf1b12e8478fd6634a31
Tripleo Nova Sci <sci@tripleo.milkyway>
2023-09-08 17:17:47 -0400

    java modules

---
+++ elijah-core/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module tripleo.elijah.core {
+	requires org.jdeferred2.core;
+	requires org.jetbrains.annotations;
+	requires antlr;
+	requires io.reactivex.rxjava3;
+	requires range.v1;
+	requires buffers.v1;
+	requires com.google.common;
+	requires org.checkerframework.checker.qual;
+	requires org.apache.commons.codec;
+	requires tripleo.elijah.nextgen.reactive;
+	requires org.apache.commons.lang3;
+	requires tripleo.elijah.stateful;
+	requires lombok;
+	requires eu.mihosoft.monacofx;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires java.desktop;
+	requires org.slf4j;
+	requires tripleo.small;
+	requires tripleo.elijah.stateful.annotation.processor;
+}
