a888e9d852bb8aa99b2828f89004d86f2e63eb9c
Tripleo Nova LP <largepoppa@tripleo.milkyway>
2023-09-20 16:42:16 -0400

    Move Hooligan to WorldModule

--- elijah-core/src/main/java/tripleo/elijah/stages/hooligan/pipeline_impl/HooliganPipelineImpl.java
+++ elijah-core/src/main/java/tripleo/elijah/stages/hooligan/pipeline_impl/HooliganPipelineImpl.java
@@ -2,7 +2,6 @@ package tripleo.elijah.stages.hooligan.pipeline_impl;
 
 import org.jetbrains.annotations.NotNull;
 import tripleo.elijah.comp.Compilation;
-import tripleo.elijah.lang.i.OS_Module;
 import tripleo.elijah.nextgen.inputtree.EIT_Input;
 import tripleo.elijah.nextgen.inputtree.EIT_ModuleInput;
 import tripleo.elijah.nextgen.outputstatement.EG_Statement;
@@ -10,14 +9,16 @@ import tripleo.elijah.nextgen.outputstatement.EX_Explanation;
 import tripleo.elijah.nextgen.outputtree.EOT_OutputFile;
 import tripleo.elijah.nextgen.outputtree.EOT_OutputTree;
 import tripleo.elijah.nextgen.outputtree.EOT_OutputType;
+import tripleo.elijah.world.i.*;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class HooliganPipelineImpl {
 	public void run(final @NotNull Compilation compilation) {
 		final Hooligan              hooligan = new Hooligan();
-		final List<OS_Module>       modules  = compilation.modules();
+		final Collection<WorldModule> modules  = compilation.livingRepo().modules();
+
 		final Hooligan.SmallWriter1 sw       = hooligan.__modules2(modules);
 		final EOT_OutputTree        cot      = compilation.getOutputTree();
 		final List<EIT_Input>       inputs   = inputs_for_modules(modules, compilation);
@@ -28,9 +29,9 @@ public class HooliganPipelineImpl {
 		cot.add(off);
 	}
 
-	private @NotNull List<EIT_Input> inputs_for_modules(final @NotNull List<OS_Module> aModules, final Compilation c) {
+	private @NotNull List<EIT_Input> inputs_for_modules(final Collection<WorldModule> aModules, final Compilation c) {
 		return aModules.stream()
-				.map(mod -> new EIT_ModuleInput(mod, c))
+				.map(WorldModule::getEITInput)
 				.collect(Collectors.toList());
 	}
 }
