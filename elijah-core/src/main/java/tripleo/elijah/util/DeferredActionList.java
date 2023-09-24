a41c4635d26e71c032dd629019afc741ce9ab20b
Tripleo Nova Sci <sci@tripleo.milkyway>
2023-09-23 21:31:04 -0400

    format: with eclipse
    - need a better pre commit hook

--- elijah-core/src/main/java/tripleo/elijah/util/DeferredActionList.java
+++ elijah-core/src/main/java/tripleo/elijah/util/DeferredActionList.java
@@ -5,15 +5,15 @@ import java.util.*;
 public class DeferredActionList<T> {
     private List<DeferredAction<T>> backing = new ArrayList<>();
 
-    DeferredAction<T> get(int index) {
+    public DeferredAction<T> get(int index) {
         return backing.get(index);
     }
 
-    void add(DeferredAction<T> item) {
+    public void add(DeferredAction<T> item) {
         backing.add(item);
     }
 
-    int size() {
+    public int size() {
         return backing.size();
     }
 }
