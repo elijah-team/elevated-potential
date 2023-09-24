6c7b5b74c305ff3e03e684b94060b8442dbbd4d2
Tripleo Nova Sci <sci@tripleo.milkyway>
2023-09-24 15:56:43 -0400

    feat: add getLongPath2

--- elijah-core/src/main/java/tripleo/elijah/comp/specs/ElijahSpec.java
+++ elijah-core/src/main/java/tripleo/elijah/comp/specs/ElijahSpec.java
@@ -2,5 +2,10 @@ package tripleo.elijah.comp.specs;
 
 import java.io.*;
 
-public record ElijahSpec(String f, File file, InputStream s, boolean do_out) {
+public record ElijahSpec(String f, File file, InputStream s) {
+	public String getLongPath2() {
+		//noinspection UnnecessaryLocalVariable
+		final String absolutePath = new File(f).getAbsolutePath(); // !!
+		return absolutePath;
+	}
 }
