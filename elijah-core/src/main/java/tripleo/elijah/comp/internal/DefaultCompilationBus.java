8a35bf07df4fbcace838d93666fc6ad85d378715
Tripleo Nova Sci <sci@tripleo.milkyway>
2023-09-28 20:24:30 -0400

    something: DefaultProgressSink

--- elijah-core/src/main/java/tripleo/elijah/comp/internal/DefaultCompilationBus.java
+++ elijah-core/src/main/java/tripleo/elijah/comp/internal/DefaultCompilationBus.java
@@ -1,10 +1,8 @@
 package tripleo.elijah.comp.internal;
 
 import org.jetbrains.annotations.*;
-import org.slf4j.*;
 import tripleo.elijah.comp.*;
 import tripleo.elijah.comp.i.*;
-import tripleo.elijah.util.*;
 
 import java.util.*;
 import java.util.concurrent.*;
@@ -12,29 +10,30 @@ import java.util.concurrent.*;
 import static tripleo.elijah.util.Helpers.*;
 
 public class DefaultCompilationBus implements ICompilationBus {
-	private static final Logger LOG = LoggerFactory.getLogger(DefaultCompilationBus.class);
-	private final CB_Monitor _monitor;
+	private final          CB_Monitor        _monitor;
 	@lombok.Getter
-	private final @NotNull CompilerDriver compilerDriver;
-	private final @NotNull Compilation c;
+	private final @NotNull CompilerDriver    compilerDriver;
+	private final @NotNull Compilation       c;
 	//	private final @NotNull List<CB_Process> _processes = new ArrayList<>();
 	@SuppressWarnings("TypeMayBeWeakened")
-	private final Queue<CB_Process> pq = new ConcurrentLinkedQueue<>();
+	private final          Queue<CB_Process> pq = new ConcurrentLinkedQueue<>();
 
-	private final @NotNull IProgressSink _defaultProgressSink = new IProgressSink() {
-		@Override
-		public void note(final Codes aCode, final @NotNull ProgressSinkComponent aProgressSinkComponent,
-		                 final int aType, final Object[] aParams) {
-			Stupidity.println_err_2(aProgressSinkComponent.printErr(aCode, aType, aParams));
-		}
-	};
+	private final @NotNull IProgressSink _defaultProgressSink;
+//	new IProgressSink() {
+//		@Override
+//		public void note(final Codes aCode, final @NotNull ProgressSinkComponent aProgressSinkComponent,
+//		                 final int aType, final Object[] aParams) {
+//			Stupidity.println_err_2(aProgressSinkComponent.printErr(aCode, aType, aParams));
+//		}
+//	};
 
 	public DefaultCompilationBus(final @NotNull CompilationEnclosure ace) {
-		c = ace.getCompilationAccess().getCompilation();
-		compilerDriver = new CompilerDriver(this);
+		c                    = ace.getCompilationAccess().getCompilation();
+		_monitor             = new CompilationRunner.__CompRunner_Monitor();
+		_defaultProgressSink = new DefaultProgressSink();
 
+		compilerDriver       = new CompilerDriver(this);
 		ace.setCompilerDriver(compilerDriver);
-		_monitor = new CompilationRunner.__CompRunner_Monitor();
 	}
 
 	@Override
@@ -63,7 +62,7 @@ public class DefaultCompilationBus implements ICompilationBus {
 				IProgressSink.Codes.LazyCompilerInstructions_inst,
 				ProgressSinkComponent.CompilationBus_,
 				-1,
-				new Object[] { aLazyCompilerInstructions.get() }
+				new Object[]{aLazyCompilerInstructions.get()}
 		);
 	}
 
@@ -80,47 +79,52 @@ public class DefaultCompilationBus implements ICompilationBus {
 	public void runProcesses() {
 		var procs = pq;
 
-		final Thread thread = new Thread(() -> {
-			LOG.debug("Polling...");
-			boolean x = true;
-			while (x) {
-				final CB_Process poll = procs.poll();
-				LOG.debug("Polled: " + poll);
-
-				if (poll != null) {
-					System.err.println("5757 "+ poll.name());
-					poll.execute(this);
-				} else {
-					System.err.println("5758 poll returned null");
-					LOG.debug("poll returned null");
-					x = false;
-				}
-			}
-		});
+		final Thread thread = new Thread(() -> __run_all_thread(procs));
 		thread.start();
-
-//		for (final CB_Process process : pq) {
-//			System.err.println("5757 "+process.name());
-//			process.execute(this);
-//		}
 
 		try {
 			thread.join();//TimeUnit.MINUTES.toMillis(1));
+
+/*
+			for (final CB_Process process : pq) {
+				System.err.println("5757 " + process.name());
+				process.execute(this);
+			}
+*/
+
 			thread.stop();
 		} catch (InterruptedException aE) {
 			throw new RuntimeException(aE);
 		}
 	}
 
+	private void __run_all_thread(final Queue<CB_Process> procs) {
+		// FIXME passing sh*t between threads (P.O.!)
+		_defaultProgressSink.note(IProgressSink.Codes.DefaultCompilationBus__pollProcess, ProgressSinkComponent.DefaultCompilationBus, 5784, new Object[]{});
+		boolean x = true;
+		while (x) {
+			final CB_Process poll = procs.poll();
+
+			if (poll != null) {
+				_defaultProgressSink.note(IProgressSink.Codes.DefaultCompilationBus__pollProcess, ProgressSinkComponent.DefaultCompilationBus, 5757, new Object[]{poll.name()});
+				poll.execute(this);
+			} else {
+				_defaultProgressSink.note(IProgressSink.Codes.DefaultCompilationBus__pollProcess, ProgressSinkComponent.DefaultCompilationBus, 5758, new Object[]{poll});
+				x = false;
+			}
+		}
+	}
+
 	static class SingleActionProcess implements CB_Process {
 		// README tape
 		private final CB_Action a;
-		private final String name;
+		private final String    name;
 
 		public SingleActionProcess(final CB_Action aAction, final String aCBFindStdLibProcess) {
-			a = aAction;
+			a    = aAction;
 			name = aCBFindStdLibProcess;
 		}
+
 		@Override
 		public @NotNull List<CB_Action> steps() {
 			return List_of(a);
