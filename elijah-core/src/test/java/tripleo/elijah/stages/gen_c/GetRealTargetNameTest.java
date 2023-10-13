222d002ca19789286e1665056cc3fb399c317c65
Tripleo Nova Sci <ics@tripleo.milkyway>
2023-10-13 04:54:21 -0400

    {+CRt} GetRealTargetNameTest

--- elijah-core/src/test/java/tripleo/elijah/stages/gen_c/GetRealTargetNameTest.java
+++ elijah-core/src/test/java/tripleo/elijah/stages/gen_c/GetRealTargetNameTest.java
@@ -6,59 +6,44 @@
  * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
  *
  */
-
 package tripleo.elijah.stages.gen_c;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import tripleo.elijah.contexts.ModuleContext;
+import org.junit.jupiter.api.*;
+import tripleo.elijah.contexts.*;
 import tripleo.elijah.lang.i.*;
-import tripleo.elijah.lang.impl.ClassStatementImpl;
-import tripleo.elijah.lang.impl.FunctionDefImpl;
-import tripleo.elijah.lang.impl.LookupResultListImpl;
-import tripleo.elijah.lang.impl.OS_ModuleImpl;
-import tripleo.elijah.stages.deduce.DeducePhase;
-import tripleo.elijah.stages.deduce.DeduceTypes2;
-import tripleo.elijah.stages.gen_fn.EvaFunction;
-import tripleo.elijah.stages.gen_fn.GenType;
-import tripleo.elijah.stages.gen_fn.GenTypeImpl;
-import tripleo.elijah.stages.gen_fn.TypeTableEntry;
-import tripleo.elijah.stages.instructions.IdentIA;
-import tripleo.elijah.stages.instructions.IntegerIA;
-import tripleo.elijah.stages.instructions.VariableTableType;
-import tripleo.elijah.test_help.Boilerplate;
-import tripleo.elijah.test_help.XX;
+import tripleo.elijah.lang.impl.*;
+import tripleo.elijah.stages.deduce.*;
+import tripleo.elijah.stages.gen_fn.*;
+import tripleo.elijah.stages.instructions.*;
+import tripleo.elijah.test_help.*;
+import tripleo.elijah.util.*;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class GetRealTargetNameTest {
-	private Boilerplate boilerPlate; // NOTE hmm. (reduce) boilerplate reductionism
-
 	EvaFunction gf;
-	OS_Module mod;
+	private Boilerplate boilerplate; // NOTE hmm. (reduce) boilerplate reductionism
 
 	@BeforeEach
-	public void setUp() throws Exception {
-		// mod = mock(OS_Module.class);
-		// FunctionDef fd = mock(FunctionDef.class);
-
-		final OS_Module mod2 = new OS_ModuleImpl();
-		final ModuleContext ctx = new ModuleContext(mod2);
+	public void setUp() {
+		final OS_Module      mod2 = new OS_ModuleImpl();
+		final ModuleContext  ctx  = new ModuleContext(mod2);
 		final ClassStatement cs = new ClassStatementImpl(mod2, ctx);
-
-		FunctionDef fd = new FunctionDefImpl(cs, ctx);
+		final ClassHeaderImpl      ch = new ClassHeaderImpl(false, Collections.emptyList());
+		ch.setName(Helpers0.string_to_ident("__FakeClassName__"));
+		cs.setHeader(ch);
+		final FunctionDef    fd = new FunctionDefImpl(cs, ctx);
 		gf = new EvaFunction(fd);
 
-		boilerPlate = new Boilerplate();
-		boilerPlate.get();
+		boilerplate = new Boilerplate();
+		boilerplate.get();
 	}
 
-	@Disabled
+	//	@Disabled
 	@Test // too complicated
-	@SuppressWarnings("JUnit3StyleTestMethodInJUnit4Class")
 	public void testManualXDotFoo() {
 		final IdentExpression x_ident   = XX.ident("x");
 		final IdentExpression foo_ident = XX.ident("foo", gf.getFD().getContext());
@@ -69,26 +54,24 @@ public class GetRealTargetNameTest {
 		// GenerateC#getRealTargetName doesn't use type information
 		// TODO but what if foo was a property instead of a member
 		//
-		final TypeTableEntry tte = xx.regularTypeName_specifyTableEntry(x_ident, gf, "X_Type");
-		final VariableStatement x_var = xx.sequenceAndVarNamed(x_ident);
-		final int int_index = gf.addVariableTableEntry("x", VariableTableType.VAR, tte, x_var);
-		final int ite_index = gf.addIdentTableEntry(foo_ident, null);
-		final IdentIA ident_ia = new IdentIA(ite_index, gf);
-		final IntegerIA integerIA = new IntegerIA(int_index, gf);
+		final TypeTableEntry    tte       = XX.regularTypeName_specifyTableEntry(x_ident, gf, "X_Type");
+		final VariableStatement x_var     = XX.sequenceAndVarNamed(x_ident);
+		final int               int_index = gf.addVariableTableEntry("x", VariableTableType.VAR, tte, x_var);
+		final int               ite_index = gf.addIdentTableEntry(foo_ident, gf.getFD().getContext());
+		final IntegerIA         integerIA = new IntegerIA(int_index, gf);
+		final IdentIA           ident_ia  = new IdentIA(ite_index, gf);
 		ident_ia.setPrev(integerIA);
 
 		Emit.emitting = false;
 
 		//
 
-		// TODO do we want silent?
+		final OS_Module mod = boilerplate.defaultMod();
+		mod.setParent(boilerplate.comp);
 
-		final OS_Module mod = boilerPlate.defaultMod();
-		mod.setParent(boilerPlate.comp);
-
-		final DeducePhase phase = boilerPlate.getDeducePhase();
+		final DeducePhase  phase        = boilerplate.getDeducePhase();
 		final DeduceTypes2 deduceTypes2 = new DeduceTypes2(mod, phase);
-		final Context ctx = mock(Context.class);
+		final Context      ctx          = mock(Context.class);
 
 		(gf.getIdentTableEntry(0)).setDeduceTypes2(deduceTypes2, ctx, gf);
 
@@ -107,10 +90,8 @@ public class GetRealTargetNameTest {
 		//
 		//
 
-		boilerPlate.getGenerateFiles(mod);
-
-		String x = ((GenerateC) boilerPlate.generateFiles).getRealTargetName(gf, ident_ia,
-				Generate_Code_For_Method.AOG.GET, null);
+		final GenerateC generateC = (GenerateC) boilerplate.getGenerateFiles2(mod);
+		final String    x         = generateC.getRealTargetName(gf, ident_ia, Generate_Code_For_Method.AOG.GET, null);
 		assertEquals("vvx->vmfoo", x);
 	}
 }
