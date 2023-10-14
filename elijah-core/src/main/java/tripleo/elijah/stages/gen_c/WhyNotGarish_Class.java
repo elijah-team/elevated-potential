150ecdf49e1120e3b592e4fd2f09bd9c90cae30d
Tripleo Nova Sci <ics@tripleo.milkyway>
2023-10-13 23:34:33 -0400

    feat: attempt to use GarishClass_Generator instead in order to reduce call sites
    - gated off (ie not tested, ie create a testing framework, plz)
    - (forgot)

--- elijah-core/src/main/java/tripleo/elijah/stages/gen_c/WhyNotGarish_Class.java
+++ elijah-core/src/main/java/tripleo/elijah/stages/gen_c/WhyNotGarish_Class.java
@@ -1,18 +1,19 @@
 package tripleo.elijah.stages.gen_c;
 
 import org.apache.commons.lang3.tuple.*;
-import org.jdeferred2.impl.DeferredObject;
-import org.jetbrains.annotations.NotNull;
+import org.jdeferred2.impl.*;
+import org.jetbrains.annotations.*;
+import tripleo.elijah.comp.*;
 import tripleo.elijah.comp.i.*;
-import tripleo.elijah.nextgen.reactive.Reactivable;
-import tripleo.elijah.nextgen.reactive.ReactiveDimension;
-import tripleo.elijah.stages.gen_fn.EvaClass;
-import tripleo.elijah.stages.gen_generic.GenerateResultEnv;
-import tripleo.elijah.stages.gen_generic.pipeline_impl.GenerateResultSink;
-import tripleo.elijah.util.NotImplementedException;
+import tripleo.elijah.nextgen.reactive.*;
+import tripleo.elijah.stages.garish.*;
+import tripleo.elijah.stages.gen_fn.*;
+import tripleo.elijah.stages.gen_generic.*;
+import tripleo.elijah.stages.gen_generic.pipeline_impl.*;
+import tripleo.elijah.util.*;
 import tripleo.elijah.world.i.*;
 
-import static tripleo.elijah.DebugFlags.MANUAL_DISABLED;
+import static tripleo.elijah.DebugFlags.*;
 
 public class WhyNotGarish_Class implements WhyNotGarish_Item {
 	public class GCFC implements Reactivable {
@@ -29,15 +30,14 @@ public class WhyNotGarish_Class implements WhyNotGarish_Item {
 		}
 	}
 
-	private final EvaClass gc;
-	private final GenerateC generateC;
-
-	private final DeferredObject<GenerateResultEnv, Void, Void> fileGenPromise = new DeferredObject<>();
-
-	private final GCFC gcfc = new GCFC();
+	private static final boolean                                       __Oct_13       = false;
+	private final        EvaClass                                      gc;
+	private final        GenerateC                                     generateC;
+	private final        DeferredObject<GenerateResultEnv, Void, Void> fileGenPromise = new DeferredObject<>();
+	private final        GCFC                                          gcfc           = new GCFC();
 
 	public WhyNotGarish_Class(final EvaClass aGc, final GenerateC aGenerateC) {
-		gc = aGc;
+		gc        = aGc;
 		generateC = aGenerateC;
 
 		gc.reactive().add(gcfc);
@@ -55,21 +55,41 @@ public class WhyNotGarish_Class implements WhyNotGarish_Item {
 	}
 
 	private void onFileGen(final @NotNull GenerateResultEnv aFileGen) {
+		//assert false;
 		NotImplementedException.raise();
 
-		if (!MANUAL_DISABLED) {
-			gcfc.respondTo(this.generateC);
-		}
+		if (__Oct_13) {
+			//final Compilation compilation = aFileGen.gmgm().gmr().mod().module().getContext().compilation();
+			final Compilation compilation = generateC._ce().getCompilation();
 
-		final LivingRepo world = generateC._ce().getCompilation().world();
-		var gc1 = world.getClass(gc).getGarish();
+			// FIXME 10/13 which result sink?
+			final GenerateResultSink resultSink2    = generateC.resultSink;
+			final GenerateResultSink resultSink     = aFileGen.resultSink();
+			final GenerateResult     generateResult = aFileGen.gr();
+			final LivingClass        livingClass    = compilation.world().getClass(gc);
+			final GarishClass        garishClass    = livingClass.getGarish();
 
-		final @NotNull GenerateResultSink sink = aFileGen.resultSink();
+			//assert resultSink == resultSink;
 
-		if (sink != null) {
-			sink.addClass_1(gc1, aFileGen.gr(), generateC);
+			gc.generator().provide(resultSink, garishClass, generateResult, generateC);
 		} else {
-			logProgress(9991, "sink failed");
+			if (!MANUAL_DISABLED) {
+				gcfc.respondTo(this.generateC);
+			}
+
+			final GenerateResult              generateResult = aFileGen.gr();
+
+			final Compilation                 compilation    = generateC._ce().getCompilation();
+			final LivingRepo                  world          = compilation.world();
+			final GarishClass                 garishClass    = world.getClass(gc).getGarish();
+
+			final @NotNull GenerateResultSink sink           = aFileGen.resultSink();
+			if (sink == null) {
+				logProgress(9991, "sink failed");
+				return;
+			}
+
+			sink.addClass_1(garishClass, generateResult, generateC);
 		}
 	}
 
