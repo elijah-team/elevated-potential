1f34fffcbf99b08122b3f2142fb7d1aa73261a3e
Tripleo Nova <tripleo@tripleo.milkyway>
2024-01-09 18:05:22 -0500

    (scary-moves) elevated-potential.core.api

--- elevated-potential.core.api/src/main/java/tripleo/elijah/comp/CPX_Signals.java
+++ elevated-potential.core.api/src/main/java/tripleo/elijah/comp/CPX_Signals.java
@@ -3,5 +3,5 @@ package tripleo.elijah.comp;
 import tripleo.elijah.comp.nextgen.i.CPX_CalculateFinishParse;
 
 public interface CPX_Signals {
-	void subscribeCalculateFinishParse(CPX_CalculateFinishParse cp_OutputPath);
+	void subscribeCalculateFinishParse(final CPX_CalculateFinishParse cp_OutputPath);
 }
