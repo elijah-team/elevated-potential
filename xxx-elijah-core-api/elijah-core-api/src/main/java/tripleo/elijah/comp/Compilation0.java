b8cbe1387f3a04abf808841f22511f553e238f6a
Tripleo Nova <tripleo@Jimothy.tripleo.tld>
2024-01-25 01:43:46 -0500

    2024-01-24 008d: core-api stuff

--- xxx-elijah-core-api/elijah-core-api/src/main/java/tripleo/elijah/comp/Compilation0.java
+++ xxx-elijah-core-api/elijah-core-api/src/main/java/tripleo/elijah/comp/Compilation0.java
@@ -1,85 +1,87 @@
 package tripleo.elijah.comp;
 
-import org.jetbrains.annotations.*;
-import tripleo.elijah.ci.*;
-import tripleo.elijah.comp.graph.i.*;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import tripleo.elijah.ci.CompilerInstructions;
+import tripleo.elijah.comp.graph.i.CK_ObjectTree;
 import tripleo.elijah.comp.i.*;
-import tripleo.elijah.comp.i.extra.*;
+import tripleo.elijah.comp.i.extra.CompilerInputListener;
 import tripleo.elijah.comp.nextgen.i.CP_Paths;
-import tripleo.elijah.comp.nextgen.pn.*;
-import tripleo.elijah.comp.nextgen.pw.*;
-import tripleo.elijah.comp.specs.*;
+import tripleo.elijah.comp.nextgen.pn.PN_Ping;
+import tripleo.elijah.comp.nextgen.pw.PW_PushWork;
+import tripleo.elijah.comp.specs.ElijahCache;
 import tripleo.elijah.g.*;
-import tripleo.elijah.lang.i.*;
-import tripleo.elijah.nextgen.inputtree.*;
-import tripleo.elijah.nextgen.outputtree.*;
-import tripleo.elijah.util.*;
+import tripleo.elijah.lang.i.OS_Package;
+import tripleo.elijah.lang.i.Qualident;
+import tripleo.elijah.nextgen.inputtree.EIT_InputTree;
+import tripleo.elijah.nextgen.outputtree.EOT_OutputTree;
+import tripleo.elijah.util.Operation2;
 
-import java.util.*;
+import java.util.List;
 
 public interface Compilation0 /*extends GCompilation*/ {
 
-    //	CompilerBeginning beginning(final CompilationRunner compilationRunner);
+	//	CompilerBeginning beginning(final CompilationRunner compilationRunner);
 
-    CK_ObjectTree getObjectTree();
+	CK_ObjectTree getObjectTree();
 
-    int errorCount();
+	int errorCount();
 
-    void feedCmdLine(@NotNull List<String> args) throws Exception;
+	void feedCmdLine(@NotNull List<String> args) throws Exception;
 
-    void feedInputs(@NotNull List<CompilerInput> inputs, CompilerController controller);
+	void feedInputs(@NotNull List<CompilerInput> inputs, CompilerController controller);
 
-    CompilationClosure getCompilationClosure();
+	CompilationClosure getCompilationClosure();
 
-    String getCompilationNumberString();
+	String getCompilationNumberString();
 
-    CompilerInputListener getCompilerInputListener();
+	CompilerInputListener getCompilerInputListener();
 
-    ErrSink getErrSink();
+	ErrSink getErrSink();
 
 
-    @Contract(pure = true)
-    List<CompilerInput> getInputs();
+	@Contract(pure = true)
+	List<CompilerInput> getInputs();
 
-    OS_Package getPackage(@NotNull Qualident pkg_name);
+	OS_Package getPackage(@NotNull Qualident pkg_name);
 
-    String getProjectName();
+	String getProjectName();
 
-    CompilerInstructions getRootCI();
+	CompilerInstructions getRootCI();
 
-    void setRootCI(CompilerInstructions aRoot);
+	void setRootCI(CompilerInstructions aRoot);
 
-    boolean isPackage(@NotNull String pkg);
+	boolean isPackage(@NotNull String pkg);
 
-    OS_Package makePackage(Qualident pkg_name);
+	OS_Package makePackage(Qualident pkg_name);
 
-    void pushItem(CompilerInstructions aci);
+	void pushItem(CompilerInstructions aci);
 
-    void use(@NotNull CompilerInstructions compilerInstructions, USE_Reasoning aReasoning);
+	void use(@NotNull CompilerInstructions compilerInstructions, USE_Reasoning aReasoning);
 
-    ElijahCache use_elijahCache();
+	ElijahCache use_elijahCache();
 
-    void pushWork(PW_PushWork aInstance, final PN_Ping aPing);
+	<Yi> void pushWork(PW_PushWork aInstance, final PN_Ping<Yi> aPing);
 
-    IO getIO();
+	IO getIO();
 
-    EOT_OutputTree getOutputTree();
+	EOT_OutputTree getOutputTree();
 
-    EIT_InputTree getInputTree();
+	EIT_InputTree getInputTree();
 
-    Finally reports();
+	Finally reports();
 
-    void subscribeCI(ICompilerInstructionsObserver aCio);
+	void subscribeCI(ICompilerInstructionsObserver aCio);
 
-    GCompilationConfig cfg();
+	GCompilationConfig cfg();
 
-    void set_pa(GPipelineAccess aPipelineAccess);
+	void set_pa(GPipelineAccess aPipelineAccess);
 
-    GCompilationEnclosure getCompilationEnclosure();
+	GCompilationEnclosure getCompilationEnclosure();
 
-    Operation2<GWorldModule> findPrelude(String aC);
+	Operation2<GWorldModule> findPrelude(String aC);
 
-    GPipelineAccess pa();
+	GPipelineAccess pa();
 
-    CP_Paths paths();
+	CP_Paths paths();
 }
--- xxx-elijah-core-api/elijah-core-api/src/main/java/tripleo/elijah/g/GCompilationEnclosure.java
+++ xxx-elijah-core-api/elijah-core-api/src/main/java/tripleo/elijah/g/GCompilationEnclosure.java
@@ -1,6 +1,7 @@
 package tripleo.elijah.g;
 
 import org.apache.commons.lang3.tuple.Pair;
+import tripleo.elijah.comp.i.CB_Output;
 import tripleo.elijah.comp.i.CompProgress;
 import tripleo.elijah.comp.i.extra.ICompilationRunner;
 import tripleo.elijah.comp.nextgen.i.AsseverationLogProgress;
@@ -13,5 +14,8 @@ public interface GCompilationEnclosure {
 	GModuleThing getModuleThing(GOS_Module aModule);
 
 	void logProgress2(CompProgress aCompProgress, AsseverationLogProgress aAsseverationLogProgress);
+
+	CB_Output getCB_Output();
+
 	ICompilationRunner getCompilationRunner();
 }
