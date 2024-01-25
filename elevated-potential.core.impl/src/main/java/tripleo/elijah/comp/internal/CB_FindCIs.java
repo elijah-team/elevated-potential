7dbf0afa7697e861290e36a93e5c3a29278860ca
Tripleo Nova <tripleo@Jimothy.tripleo.tld>
2024-01-25 01:41:45 -0500

    2024-01-24 008: Enprettify CB_FindCIs

--- elevated-potential.core.impl/src/main/java/tripleo/elijah/comp/internal/CB_FindCIs.java
+++ elevated-potential.core.impl/src/main/java/tripleo/elijah/comp/internal/CB_FindCIs.java
@@ -3,7 +3,6 @@ package tripleo.elijah.comp.internal;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import tripleo.elijah.comp.CompilerInput;
-//import tripleo.elijah.comp.internal_move_soon.CompilationEnclosure;
 import tripleo.elijah.comp.i.CB_Action;
 import tripleo.elijah.comp.i.CB_Monitor;
 import tripleo.elijah.comp.i.CB_Output;
@@ -14,6 +13,7 @@ import tripleo.elijah.comp.i.extra.ICompilationRunner;
 import tripleo.elijah.comp.percy.CN_CompilerInputWatcher;
 import tripleo.elijah.nextgen.comp_model.CM_CompilerInput;
 import tripleo.elijah.util.Maybe;
+import tripleo.elijah.util.SimplePrintLoggerToRemoveSoon;
 import tripleo.wrap.File;
 
 import java.nio.file.NotDirectoryException;
@@ -22,7 +22,7 @@ import java.util.List;
 class CB_FindCIs implements CB_Action {
 	private final List<CompilerInput> _inputs;
 	private final CB_Output           o;
-	private final CompilationClosure cc;
+	private final CompilationClosure  cc;
 
 	@Contract(pure = true)
 	public CB_FindCIs(final ICompilationRunner aCompilationRunner, final List<CompilerInput> aInputs) {
@@ -37,7 +37,7 @@ class CB_FindCIs implements CB_Action {
 
 		// cilly is this, aka use CIL
 		//ce.getCompilerInput()
-				_inputs
+		_inputs
 				.stream().filter(input -> input.ty() != CompilerInput.Ty.ARG) // skip if in #_processInput
 				.forEach(input -> _processInput(cc, input));
 
@@ -71,19 +71,22 @@ class CB_FindCIs implements CB_Action {
 			}
 
 			CW_inputIsEzFile.apply(input, compilationClosure);
+		} else if (f.isDirectory()) {
+			// errSink.reportError("9996 Not an .ez file "+file_name);
+			final CompilationImpl compilation = (CompilationImpl) compilationClosure.getCompilation();
+
+			// FIXME 24/01/09 Duplication alert??
+			compilation.addCompilerInputWatcher(CB_FindCIs::__CN_CompilerInputWatcher__event);
+			CW_inputIsDirectory.apply(input, compilationClosure, f);
 		} else {
 			// errSink.reportError("9996 Not an .ez file "+file_name);
-			if (f.isDirectory()) {
-				final CompilationImpl compilation = (CompilationImpl) compilationClosure.getCompilation();
-
-				// FIXME 24/01/09 Duplication alert??
-				compilation.addCompilerInputWatcher(CB_FindCIs::__CN_CompilerInputWatcher__event);
-				CW_inputIsDirectory.apply(input, compilationClosure, f);
-			} else {
-				final NotDirectoryException d = new NotDirectoryException(f.toString());
-				errSink.reportError("9995 Not a directory " + f.getAbsolutePath());
-			}
+			final NotDirectoryException d = new NotDirectoryException(f.toString());
+			errSink.reportError("9995 Not a directory " + f.getAbsolutePath());
 		}
+	}
+
+	private void logProgress_Stating(final String aSection, final String aStatement) {
+		SimplePrintLoggerToRemoveSoon.println_out_3("** CB_FindCIs :: %s :: %s".formatted(aSection, aStatement));
 	}
 
 	private static void __CN_CompilerInputWatcher__event(final CN_CompilerInputWatcher.e aEvent, final CompilerInput aCompilerInput, final Object aObject) {
@@ -100,9 +103,5 @@ class CB_FindCIs implements CB_Action {
 			System.err.println("~~ [11/24 111] " + aEvent + " " + aCompilerInput);
 		}
 		}
-	}
-
-	private void logProgress_Stating(final String aSection, final String aStatement) {
-		tripleo.elijah.util.SimplePrintLoggerToRemoveSoon.println_out_3("** CB_FindCIs :: %s :: %s".formatted(aSection, aStatement));
 	}
 }
