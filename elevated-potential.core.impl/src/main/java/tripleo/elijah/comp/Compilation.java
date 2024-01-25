2acce6e057d4e54f1256cdf6813a00b02a240cef
Tripleo Nova <tripleo@Jimothy.tripleo.tld>
2024-01-25 01:02:44 -0500

    2024-01-24 005: Stop overspecifying and remove unused
    - Mandatory reformat

--- elevated-potential.core.impl/src/main/java/tripleo/elijah/comp/Compilation.java
+++ elevated-potential.core.impl/src/main/java/tripleo/elijah/comp/Compilation.java
@@ -4,21 +4,18 @@ import io.reactivex.rxjava3.core.Observer;
 import org.jetbrains.annotations.NotNull;
 import tripleo.elijah.ci.CompilerInstructions;
 import tripleo.elijah.comp.graph.CM_Ez;
-import tripleo.elijah.compiler_model.CM_Module;
-import tripleo.elijah.comp.i.*;
+import tripleo.elijah.comp.i.CompFactory;
+import tripleo.elijah.comp.i.LCM_CompilerAccess;
+import tripleo.elijah.comp.i.extra.ICompilationRunner;
 import tripleo.elijah.comp.i.extra.IPipelineAccess;
 import tripleo.elijah.comp.internal.*;
-import tripleo.elijah.comp.internal_move_soon.CompilationEnclosure;
-import tripleo.elijah.comp.nextgen.i.CP_Paths;
-import tripleo.elijah.comp.nextgen.pn.PN_Ping;
-import tripleo.elijah.comp.nextgen.pw.PW_PushWork;
 import tripleo.elijah.comp.percy.CN_CompilerInputWatcher;
-import tripleo.elijah.comp.specs.*;
+import tripleo.elijah.comp.specs.ElijahSpec;
+import tripleo.elijah.comp.specs.EzSpec;
+import tripleo.elijah.compiler_model.CM_Module;
 import tripleo.elijah.g.GCompilationConfig;
-import tripleo.elijah.g.GWorldModule;
 import tripleo.elijah.lang.i.OS_Module;
 import tripleo.elijah.nextgen.comp_model.CM_CompilerInput;
-import tripleo.elijah.stages.deduce.fluffy.i.FluffyComp;
 import tripleo.elijah.util.*;
 import tripleo.elijah.world.i.LivingRepo;
 import tripleo.elijah_elevated.comp.model.CM_ModelFactory;
@@ -29,26 +26,11 @@ import java.util.List;
 public interface Compilation extends Compilation0 {
 	CM_Module megaGrande(OS_Module aModule);
 
-	LivingRepo world2();
-
-	Operation<Ok> hasInstructions2(@NotNull List<CompilerInstructions> cis, @NotNull IPipelineAccess pa);
-
-	@Override
-	IPipelineAccess pa();
-
-	@Override
-	Operation2<GWorldModule> findPrelude(String prelude_name);
-
 	IPipelineAccess get_pa();
 
 	void set_pa(IPipelineAccess a_pa);
 
 	void addCompilerInputWatcher(CN_CompilerInputWatcher aCNCompilerInputWatcher);
-
-	void compilerInputWatcher_Event(CN_CompilerInputWatcher.e aEvent, CompilerInput aCompilerInput, Object aO);
-
-	@Override
-	CompilationEnclosure getCompilationEnclosure();
 
 	CIS _cis();
 
@@ -56,40 +38,13 @@ public interface Compilation extends Compilation0 {
 
 	void setIO(IO io);
 
-	@NotNull
-	FluffyComp getFluffy();
-
 	Operation<Ok> hasInstructions(@NotNull List<CompilerInstructions> cis, @NotNull IPipelineAccess pa);
 
 	ModuleBuilder moduleBuilder();
 
-	@Override
-	CP_Paths paths();
-
-	@Override
-	void pushItem(CompilerInstructions aci);
-
-	@Override
-	Finally reports();
-
 	void subscribeCI(@NotNull Observer<CompilerInstructions> aCio);
 
-	@Override
-	CompilationConfig cfg();
-
-	@Override
-	List<CompilerInput> getInputs();
-
-	@Override
-	void use(@NotNull CompilerInstructions compilerInstructions, USE_Reasoning aReasoning);
-
 	LivingRepo world();
-
-	@Override
-	ElijahCache use_elijahCache();
-
-	@Override
-	void pushWork(PW_PushWork aInstance, PN_Ping aPing);
 
 	CM_Module megaGrande(ElijahSpec aSpec, Operation2<OS_Module> aModuleOperation);
 
@@ -97,7 +52,7 @@ public interface Compilation extends Compilation0 {
 
 	LCM_CompilerAccess getLCMAccess();
 
-	CompilationRunner getRunner();
+	ICompilationRunner getRunner();
 
 	CompilationConfig _cfg();
 
@@ -113,9 +68,11 @@ public interface Compilation extends Compilation0 {
 
 	LCM lcm();
 
+	CPX_Signals signals();
+
 	class CompilationConfig implements GCompilationConfig {
-		public          boolean showTree = false;
-		public          boolean silent   = false;
+		public boolean showTree = false;
+		public boolean silent   = false;
 
 		@Override
 		public void setSilent(final boolean b) {
@@ -124,5 +81,33 @@ public interface Compilation extends Compilation0 {
 
 	}
 
-	CPX_Signals signals();
+	//void compilerInputWatcher_Event(CN_CompilerInputWatcher.e aEvent, CompilerInput aCompilerInput, Object aO);
+
+	//LivingRepo world2();
+
+	//@Override CP_Paths paths();
+
+	//@Override void pushItem(CompilerInstructions aci);
+
+	//@Override Finally reports();
+
+	//@Override List<CompilerInput> getInputs();
+
+	//@Override CompilationConfig cfg();
+
+	//@Override <Yi> void pushWork(PW_PushWork aInstance, PN_Ping<Yi> aPing);
+
+	//@Override void use(@NotNull CompilerInstructions compilerInstructions, USE_Reasoning aReasoning);
+
+	//@Override ElijahCache use_elijahCache();
+
+	//@Override CompilationEnclosure getCompilationEnclosure();
+
+	//Operation<Ok> hasInstructions2(@NotNull List<CompilerInstructions> cis, @NotNull IPipelineAccess pa);
+
+	//@Override IPipelineAccess pa();
+
+	//@Override Operation2<GWorldModule> findPrelude(String prelude_name);
+
+	//@NotNull FluffyComp getFluffy();
 }
