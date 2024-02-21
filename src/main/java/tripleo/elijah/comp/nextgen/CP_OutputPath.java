ef098d27447a3ac078c72fc8f63b931e295ca4c4
tripleo <tripleo@tripleo.tripleo>
2024-02-21 07:01:05 -0500

    Move __PathPromiseCalculator *class* to `CP_OutputPathImpl`

--- src/main/java/tripleo/elijah/comp/nextgen/CP_OutputPath.java
+++ src/main/java/tripleo/elijah/comp/nextgen/CP_OutputPath.java
@@ -16,8 +16,6 @@ import tripleo.elijah_elevated.gu.R;
 import tripleo.wrap.File;
 
 import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public interface CP_OutputPath extends /*CP_Path,*/ _CP_RootPath, CPX_CalculateFinishParse, R.R_Hook {
@@ -78,32 +76,4 @@ public interface CP_OutputPath extends /*CP_Path,*/ _CP_RootPath, CPX_CalculateF
 
 	@Override
 	void notify_CPX_CalculateFinishParse(Ok ok);
-
-	public static class __PathPromiseCalculator {
-		private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss");
-		private       String            date;
-		private       String            c_name;
-
-		public String c_name() {
-			return c_name;
-		}
-
-		public void calc(String c_name) {
-			final LocalDateTime localDateTime = LocalDateTime.now();
-			final String        date          = formatter.format(localDateTime); // 15-02-2022 12:43
-
-			this.c_name = c_name;
-			this.date   = date;
-		}
-
-		public String date() {
-			return date;
-		}
-
-		public CP_Path getP(final @NotNull CP_OutputPathImpl aCPOutputPath) {
-			final CP_Path outputRoot = aCPOutputPath.paths_outputRoot();
-
-			return outputRoot.child(c_name).child(date);
-		}
-	}
 }
