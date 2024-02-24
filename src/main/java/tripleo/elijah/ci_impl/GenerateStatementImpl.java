c774722aaa728333d30fec647a0a1688b5675a41
tripleo <tripleo@tripleo.tripleo>
2024-02-24 14:02:08 -0500

    Add GenerateStatement#dirStream

--- src/main/java/tripleo/elijah/ci_impl/GenerateStatementImpl.java
+++ src/main/java/tripleo/elijah/ci_impl/GenerateStatementImpl.java
@@ -1,29 +1,25 @@
-/*
- * Elijjah compiler, copyright Tripleo <oluoluolu+elijah@gmail.com>
- *
- * The contents of this library are released under the LGPL licence v3,
- * the GNU Lesser General Public License text was downloaded from
- * http://www.gnu.org/licenses/lgpl.html from `Version 3, 29 June 2007'
- *
- */
 package tripleo.elijah.ci_impl;
 
-import antlr.Token;
-import org.jetbrains.annotations.NotNull;
 import tripleo.elijah.ci.CiExpression;
 import tripleo.elijah.ci.GenerateStatement;
 import tripleo.elijah.xlang.LocatableString;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Created 9/6/20 12:04 PM
  */
 public class GenerateStatementImpl implements GenerateStatement {
 	@Override
-	public void addDirective(final @NotNull Token token, final CiExpression expression) {
-		dirs.add(new Directive(LocatableString.of(token), expression));
+	public void addDirective(LocatableString token, CiExpression expression) {
+		dirs.add(new Directive(token, expression));
+	}
+
+	@Override
+	public Stream<Directive> dirStream() {
+		return dirs.stream();
 	}
 
 	public final List<Directive> dirs = new ArrayList<Directive>();
