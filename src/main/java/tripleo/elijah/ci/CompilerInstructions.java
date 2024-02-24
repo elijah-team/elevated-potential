ccc57d73b6e579e1348c3c727093980fd7167f9e
tripleo <tripleo@tripleo.tripleo>
2024-02-24 13:37:58 -0500

    Update from rolling-0109c+0206c
    - Stealing from 853e802a5672b7acde30742ab530633d0da85eab
    
    - irony: Jumped around 45 times, forgot exactly what's going on

--- src/main/java/tripleo/elijah/ci/CompilerInstructions.java
+++ src/main/java/tripleo/elijah/ci/CompilerInstructions.java
@@ -1,6 +1,6 @@
 package tripleo.elijah.ci;
 
-import tripleo.elijah.comp.i.CompilerInput;
+import tripleo.elijah.comp.inputs.CompilerInput;
 import tripleo.elijah.compiler_model.CM_Filename;
 import tripleo.elijah.xlang.LocatableString;
 import tripleo.wrap.File;
