9dc7cd6d7b1713c0ce0544e303b69989caba6afd
tripleo <tripleo@tripleo.tripleo>
2024-02-24 08:43:53 -0500

    [minor] Elaborate

--- src/main/java/tripleo/elijah_elevated/comp/W.java
+++ src/main/java/tripleo/elijah_elevated/comp/W.java
@@ -2,8 +2,15 @@ package tripleo.elijah_elevated.comp;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.function.Supplier;
 
+import org.jetbrains.annotations.NotNull;
 import tripleo.elijah.comp.IO;
+import tripleo.elijah.comp.i.ICompilationAccess;
+import tripleo.elijah.comp.i.ICompilationBus;
+import tripleo.elijah.comp.impl.DefaultCompilationEnclosure;
+import tripleo.elijah.comp.internal_move_soon.CompilationEnclosure;
+import tripleo.elijah.g.GCompilationEnclosure;
 import tripleo.elijah.util.Operation;
 import tripleo.wrap.File;
 
@@ -15,4 +22,14 @@ public class W {
 			return Operation.failure(aE);
 		}
 	}
+
+    @NotNull
+    public static Supplier<ICompilationBus> extractCompilationBus(@NotNull ICompilationAccess aca) {
+        return () -> {
+			final GCompilationEnclosure gCompilationEnclosure  = aca.getCompilation().getCompilationEnclosure();
+			final CompilationEnclosure  compilationEnclosure = (DefaultCompilationEnclosure) gCompilationEnclosure;
+			final ICompilationBus       compilationBus        = compilationEnclosure.getCompilationBus();
+			return compilationBus;
+		};
+    }
 }
