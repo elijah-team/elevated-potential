f4f1d08638d0edb4c655e3cbe6a3605da17197aa
Tripleo <noble@tripleo.nonexistent>
2024-08-08 18:33:52 -0400

    "Just" remove it

--- elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/gen_fn/BaseEvaFunction.java
+++ elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/gen_fn/BaseEvaFunction.java
@@ -8,7 +8,6 @@
  */
 package tripleo.elijah.stages.gen_fn;
 
-import org.jdeferred2.*;
 import org.jetbrains.annotations.*;
 import tripleo.elijah.*;
 import tripleo.elijah.lang.i.*;
@@ -263,11 +262,6 @@ public abstract class BaseEvaFunction
 	}
 
 	@Override
-	public int getCode() {
-		return code;
-	}
-
-	@Override
 	@NotNull
 	public ConstantTableEntry getConstTableEntry(final int index) {
 		return cte_list.get(index);
@@ -316,7 +310,6 @@ public abstract class BaseEvaFunction
 	/**
 	 * Returns a string that represents the path encoded by ia2. Does not transform
 	 * the string into target language (ie C). Called from
-	 * {@link DeduceTypes2#do_assign_call(BaseEvaFunction, Context, IdentTableEntry, FnCallArgs, int)}
 	 * or {@link DeduceTypes2#deduce_generated_function(EvaFunction, ModuleThing)}
 	 * or
 	 * {@link DeduceTypes2#resolveIdentIA_(Context, IdentIA, BaseEvaFunction, FoundElement)}
@@ -331,7 +324,7 @@ public abstract class BaseEvaFunction
 		//
 		// TODO NOT LOOKING UP THINGS, IE PROPERTIES, MEMBERS
 		//
-		List<String> sl = new ArrayList<String>();
+		List<String> sl = new ArrayList<>();
 		for (final InstructionArgument ia : s) {
 			final String text;
 			if (ia instanceof IntegerIA) {
@@ -352,7 +345,7 @@ public abstract class BaseEvaFunction
 	}
 
 	public static @NotNull List<InstructionArgument> _getIdentIAPathList(@NotNull InstructionArgument oo) {
-		LinkedList<InstructionArgument> s = new LinkedList<InstructionArgument>();
+		LinkedList<InstructionArgument> s = new LinkedList<>();
 		while (oo != null) {
 			if (oo instanceof IntegerIA) {
 				s.addFirst(oo);
@@ -473,18 +466,15 @@ public abstract class BaseEvaFunction
 
 	@Override
 	public void resolveTypeDeferred(final @NotNull GenType aType) {
-		if (_p_assignGenType.isPending())
+		if (_p_assignGenType.isPending()) {
 			_p_assignGenType.resolve(aType);
-		else {
-			final Holder<GenType> holder = new Holder<GenType>();
-			_p_assignGenType.then(new DoneCallback<GenType>() {
-				@Override
-				public void onDone(final GenType result) {
-					holder.set(result);
-				}
-			});
-			tripleo.elijah.util.SimplePrintLoggerToRemoveSoon.println_err_2(String.format("Trying to resolve function twice 1) %s 2) %s",
-																	  holder.get().asString(), aType.asString()));
+		} else {
+			final Holder<GenType> holder = new Holder<>();
+			_p_assignGenType.then(holder::set);
+			final String s = String.format("Trying to resolve function twice 1) %s 2) %s",
+										   holder.get().asString(),
+										   aType.asString());
+			tripleo.elijah.util.SimplePrintLoggerToRemoveSoon.println_err_2(s);
 		}
 	}
 
@@ -492,11 +482,13 @@ public abstract class BaseEvaFunction
 	public void setClass(@NotNull EvaNode aNode) {
 		assert aNode instanceof EvaClass || aNode instanceof EvaNamespace;
 
+/*
 		//assert ((EvaContainerNC) aNode).getCode() != 0;
 		if (((EvaContainerNC) aNode).getCode() == 0) {
 			//throw new AssertionError();
 			tripleo.elijah.util.SimplePrintLoggerToRemoveSoon.println_err_4("504504 node is not coded in setClass "+aNode.identityString());
 		}
+*/
 
 		genClass = aNode;
 
