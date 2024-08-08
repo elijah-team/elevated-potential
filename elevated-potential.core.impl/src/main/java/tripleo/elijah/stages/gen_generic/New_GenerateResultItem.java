d79ef689e4fc0f32fc1db8bfb664af6e012ace60
Tripleo <noble@tripleo.nonexistent>
2024-08-08 18:28:31 -0400

    ??

--- elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/gen_generic/New_GenerateResultItem.java
+++ elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/gen_generic/New_GenerateResultItem.java
@@ -1,96 +1,78 @@
 package tripleo.elijah.stages.gen_generic;
 
-import io.reactivex.rxjava3.core.Observer;
-import lombok.experimental.*;
 import org.jetbrains.annotations.*;
 import tripleo.elijah.ci.*;
-import tripleo.elijah.stages.gen_c.*;
 import tripleo.elijah.stages.gen_fn.*;
 import tripleo.util.buffer.*;
 
 import java.util.*;
-import java.util.function.*;
 
 public class New_GenerateResultItem implements GenerateResultItem {
-	@Delegate
-	private final Old_GenerateResult ogri;
+	private final Old_GenerateResultItem ogri;
 
-	public New_GenerateResultItem() {
-		ogri = new Old_GenerateResult();
+	public New_GenerateResultItem(final GenerateResult.@NotNull TY aTy,
+								  final @NotNull Buffer aBuffer,
+								  final @NotNull EvaNode aNode,
+								  final @NotNull LibraryStatementPart aLsp,
+								  final @NotNull Dependency aDependency,
+								  final int aCounter) {
+		ogri = new Old_GenerateResultItem(aTy, aBuffer, aNode, aLsp, aDependency, aCounter);
 	}
 
-	public void add(final @NotNull Buffer b, final @NotNull EvaNode n, final GenerateResult.@NotNull TY ty, final @Nullable LibraryStatementPart aLsp, @NotNull final Dependency d) {
-		ogri.add(b, n, ty, aLsp, d);
+	@Override
+	public GenerateResult.@NotNull TY __ty() {
+		return ogri.__ty();
 	}
 
-	public void addClass(final GenerateResult.@NotNull TY ty,
-						 final @NotNull EvaClass aEvaClass,
-						 final @NotNull Buffer aBuf,
-						 final LibraryStatementPart aLsp) {
-		assert aEvaClass inms
-		ogri.addClass(ty, aEvaClass, aBuf, aLsp);
+	@Override
+	public int _counter() {
+		return ogri._counter();
 	}
 
-	public void addConstructor(final @NotNull EvaConstructor aEvaConstructor,
-							   final @NotNull Buffer aBuffer,
-							   final GenerateResult.@NotNull TY aTY,
-							   final LibraryStatementPart aLsp) {
-		assert aEvaConstructor instanceof DeducedEvaConstructor;
-		ogri.addConstructor(aEvaConstructor, aBuffer, aTY, aLsp);
+	@Override
+	@NotNull
+	public Buffer buffer() {
+		return ogri.buffer();
 	}
 
-	public void addFunction(final @NotNull BaseEvaFunction aGeneratedFunction,
-							final @NotNull Buffer aBuffer,
-							final GenerateResult.@NotNull TY aTY,
-							final LibraryStatementPart aLsp) {
-		ogri.addFunction(aGeneratedFunction, aBuffer, aTY, aLsp);
+	@Override
+	@NotNull
+	public List<DependencyRef> dependencies() {
+		return ogri.dependencies();
 	}
 
-	public void additional(final @NotNull GenerateResult aGenerateResult) {
-		ogri.additional(aGenerateResult);
+	@Override
+	@NotNull
+	public Dependency getDependency() {
+		return ogri.getDependency();
 	}
 
-	public void addNamespace(final GenerateResult.@NotNull TY ty,
-							 final @NotNull EvaNamespace aNamespace,
-							 final @NotNull Buffer aBuf,
-							 final LibraryStatementPart aLsp) {
-		assert aNamespace instanceof DeducedEvaNamespace;
-		ogri.addNamespace(ty, aNamespace, aBuf, aLsp);
+	@Override
+	@NotNull
+	public String jsonString() {
+		return ogri.jsonString();
 	}
 
-	public void addWatcher(final IGenerateResultWatcher w) {
-		ogri.addWatcher(w);
+	@Override
+	@NotNull
+	public LibraryStatementPart lsp() {
+		return ogri.lsp();
 	}
 
-	public void close() {
-		ogri.close();
+	@Override
+	@NotNull
+	public EvaNode node() {
+		return ogri.node();
 	}
 
-	public void completeItem(final GenerateResultItem aGenerateResultItem) {
-		ogri.completeItem(aGenerateResultItem);
+	@Override
+	public String output() {
+		return ogri.output();
 	}
 
-	public void observe(final @NotNull Observer<GenerateResultItem> obs) {
-		ogri.observe(obs);
-	}
-
-	public void outputFiles(final @NotNull Consumer<Map<String, OutputFileC>> cmso) {
-		ogri.outputFiles(cmso);
-	}
-
-	public @NotNull List<Old_GenerateResultItem> results() {
-		return ogri.results();
-	}
-
-	public void signalDone() {
-		ogri.signalDone();
-	}
-
-	public void signalDone(final Map<String, OutputFileC> aOutputFiles) {
-		ogri.signalDone(aOutputFiles);
-	}
-
-	public void subscribeCompletedItems(final @NotNull Observer<GenerateResultItem> aGenerateResultItemObserver) {
-		ogri.subscribeCompletedItems(aGenerateResultItemObserver);
+	@Override
+	@NotNull
+	public String toString() {
+		return ogri.toString();
 	}
 }
