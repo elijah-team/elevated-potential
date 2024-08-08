4e000bc597b0a441fd94383601e63a087c13b37a
Tripleo <noble@tripleo.nonexistent>
2024-08-08 16:44:19 -0400

    Add Eventual shortcut
    1. Will register with DeducePhase
    2. Overrides #description for descriptiveness
    3. Finally
    4. Prob would have used "injector" before

--- elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
+++ elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/deduce/DeduceTypes2.java
@@ -23,6 +23,7 @@ import tripleo.elijah.comp.i.*;
 import tripleo.elijah.comp.i.extra.*;
 import tripleo.elijah.diagnostic.*;
 import tripleo.elijah.g.*;
+import tripleo.elijah.Eventual;
 import tripleo.elijah.lang.i.*;
 import tripleo.elijah.lang.impl.*;
 import tripleo.elijah.lang.nextgen.names.i.*;
@@ -1499,68 +1500,15 @@ public class DeduceTypes2 implements GDeduceTypes2 {
 		}
 	}
 
-	public enum ClassInvocationMake {
-		;
-
-		public static @NotNull Operation<ClassInvocation> withGenericPart(@NotNull ClassStatement best,
-																		  String constructorName, @Nullable NormalTypeName aTyn1, @NotNull DeduceTypes2 dt2) {
-			if (aTyn1 == null) {
-				// throw new IllegalStateException("blank typename");
+	public <T> Eventual<T> eventual(final String aDescription) {
+		final Eventual<T> result = new Eventual<T>() {
+			@Override
+			public String description() {
+				return aDescription;
 			}
-
-			@NotNull
-			GenericPart genericPart = dt2._inj().new_GenericPart(best, aTyn1);
-
-			@Nullable
-			ClassInvocation clsinv = dt2._inj().new_ClassInvocation(best, constructorName, new ReadySupplier_1<>(dt2));
-
-			if (genericPart.hasGenericPart()) {
-				final @NotNull List<TypeName> gp  = best.getGenericPart();
-				final @Nullable TypeNameList  gp2 = genericPart.getGenericPartFromTypeName();
-
-				if (gp2 != null) {
-					for (int i = 0; i < gp.size(); i++) {
-						final TypeName typeName = gp2.get(i);
-						@NotNull
-						GenType typeName2;
-						try {
-							typeName2 = dt2.resolve_type(dt2._inj().new_OS_UserType(typeName), typeName.getContext());
-							// TODO transition to GenType
-							clsinv.set(i, gp.get(i), typeName2.getResolved());
-						} catch (ResolveError aResolveError) {
-							return Operation.failure(aResolveError);
-						}
-					}
-				}
-			}
-			return Operation.success(clsinv);
-		}
-
-		public static DeduceTypes2Injector.DerivedClassInvocation2 withGenericPart2(final ClassStatement aClassStatement, final String aO, final NormalTypeName aNormalTypeName, final DeduceTypes2 aDeduceTypes2) {
-			@NotNull final Operation<ClassInvocation> x = withGenericPart(aClassStatement, aO, aNormalTypeName, aDeduceTypes2);
-			final DeduceTypes2Injector.DerivedClassInvocation2 result = new DeduceTypes2Injector.DerivedClassInvocation2() {
-				@Override
-				public void into(final Eventual<IInvocation> ev) {
-					switch (x.mode()) {
-					case SUCCESS -> {
-						ev.resolve(x.success());
-					}
-					case FAILURE -> {
-						final Throwable           failure = x.failure();
-						final ExceptionDiagnostic ed      = new ExceptionDiagnostic(failure);
-						ev.reject(ed);
-					}
-					default -> {
-						// hmm!
-						final Throwable           failure = new IllegalStateException("Unexpected value: " + x.mode());
-						final ExceptionDiagnostic ed      = new ExceptionDiagnostic(failure);
-						ev.reject(ed);
-					}
-					}
-				}
-			};
-			return result;
-		}
+		};
+		result.register(this._phase());
+		return result;
 	}
 
 	public enum ProcessElement {
