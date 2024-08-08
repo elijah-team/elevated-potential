0d214c8d2b482f647c67790f71f148529f56c83b
Tripleo <noble@tripleo.nonexistent>
2024-08-08 19:35:21 -0400

    unlower WhyNotGarish_Item

--- elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/gen_c/WhyNotGarish_Item.java
+++ elevated-potential.core.impl/src/main/java/tripleo/elijah/stages/gen_c/WhyNotGarish_Item.java
@@ -1,9 +1,16 @@
 package tripleo.elijah.stages.gen_c;
 
-import tripleo.elijah.stages.gen_generic.GenerateResultEnv;
+import tripleo.elijah.stages.gen_generic.*;
 
 interface WhyNotGarish_Item {
 	boolean hasFileGen();
 
 	void provideFileGen(GenerateResultEnv fg);
+
+	/**
+	 * OppositeOfLower??
+	 * <p>
+	 * thinking about rust (not wayland)/llvm
+	 */
+	DeducedEvaNode ool();
 }
